3e8f20d9a156a2fce924cb8ccfde379965bdf3c6
TechSupport <76240748+TechAssistance@users.noreply.github.com>
2023-12-17 08:48:43 -0700

    Create ParkingSystem.java

---
+++ Leetcode/ParkingSystem.java
@@ -0,0 +1,55 @@
+class ParkingSystem {
+    private int big;
+    private int medium;
+    private int small;
+
+    public ParkingSystem(int big, int medium, int small) {
+        this.big = big;
+        this.medium = medium;
+        this.small = small;
+    }
+
+    private boolean checkAvailability(int carType) {
+        switch (carType) {
+            case 1:
+                return big > 0;
+            case 2:
+                return medium > 0;
+            case 3:
+                return small > 0;
+            default:
+                return false;
+        }
+    }
+
+    private boolean parkCar(int carType) {
+        switch (carType) {
+            case 1:
+                if (big > 0) {
+                    big--;
+                    return true;
+                }
+                break;
+            case 2:
+                if (medium > 0) {
+                    medium--;
+                    return true;
+                }
+                break;
+            case 3:
+                if (small > 0) {
+                    small--;
+                    return true;
+                }
+                break;
+        }
+        return false;
+    }
+
+    public boolean addCar(int carType) {
+        if (carType >= 1 && carType <= 3) {
+            return parkCar(carType);
+        }
+        return false;
+    }
+}
